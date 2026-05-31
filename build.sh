10e585fab11c785ca5eee71e62110bc70ca861bd
Lunar <lunarlifeburst@gmail.com>
2026-05-31 22:38:37 +1000

    cute progress bar and compressed output

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 buildah build \
   --layers \
   -f Containerfile \
-  --output type=tar,dest=./sdk-rootfs.tar \
-  .
+  --output type=tar,dest=/dev/stdout \
+  . | zstd -10 -v -o sdk-rootfs.tar.zst
