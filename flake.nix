a5ce2758d600e9f930505f4ed0d8bfa99b80df94
Lunar <lunarlifeburst@gmail.com>
2026-06-04 18:26:08 +1000

    move --target to flags

--- flake.nix
+++ flake.nix
@@ -58,9 +58,10 @@
         shellHook = ''
           export LITOLI_SDK="$LITOLI_SDK"
           export TARGET="x86_64-linux-gnu"
-          export CC="${clang'}/bin/clang --target=$TARGET"
-          export CXX="${clang'}/bin/clang++ --target=$TARGET"
+          export CC="${clang'}/bin/clang"
+          export CXX="${clang'}/bin/clang++"
           export AS="$CC"
+          export ASFLAGS="--target=$TARGET"
           export LD="$CC"
           export AR=${binutils'}/bin/llvm-ar
           export RANLIB=${binutils'}/bin/llvm-ranlib
@@ -68,9 +69,9 @@
           export STRIP=${binutils'}/bin/llvm-strip
           export INCLUDEDIRS="$LITOLI_SDK/usr/include"
           export LIBDIRS="$LITOLI_SDK/usr/lib/x86_64-linux-gnu"
-          export CFLAGS="-I$INCLUDEDIRS --sysroot=$LITOLI_SDK"
-          export CXXFLAGS="-I$INCLUDEDIRS --sysroot=$LITOLI_SDK"
-          export LDFLAGS="-fuse-ld=lld -Wl,--allow-shlib-undefined -Wl,--dynamic-linker=/lib64/ld-linux-x86-64.so.2 -L$LIBDIRS"
+          export CFLAGS="-I$INCLUDEDIRS --sysroot=$LITOLI_SDK --target=$TARGET"
+          export CXXFLAGS="-I$INCLUDEDIRS --sysroot=$LITOLI_SDK --target=$TARGET"
+          export LDFLAGS="-fuse-ld=lld -Wl,--allow-shlib-undefined -Wl,--dynamic-linker=/lib64/ld-linux-x86-64.so.2 -L$LIBDIRS --target=$TARGET"
           
           export PKG_CONFIG_SYSROOT_DIR="$LITOLI_SDK"
           export PKG_CONFIG_LIBDIR="$LITOLI_SDK/usr/lib/pkgconfig:$LITOLI_SDK/usr/share/pkgconfig:$LITOLI_SDK/usr/lib/x86_64-linux-gnu/pkgconfig"
